c03ba04201aa952231df426e9a178d77deeb01e4
kwabenakimia <80727231+kwabenakimia@users.noreply.github.com>
2024-07-13 15:10:59 +0000

    add wait_for_database.sh to run the database in the background

--- web_app/scripts/wait_for_database.sh
+++ web_app/scripts/wait_for_database.sh
@@ -3,11 +3,14 @@
 cd ..
 docker-compose up -d
 
-until pg_isready -h localhost -p 5432 -U username
-do
-  echo "Waiting for postgres"
-  sleep 2;
-done
+#until docker run -it postgres --add-host host.docker.internal:host-gateway \
+#    docker.io/postgres:14-alpine -h localhost -p 5433 -U sophia pg_isready
+./scripts/wait-for-it.sh -t 4 -h localhost -p 5433
+#until docker run -it --rm --add-host host.docker.internal:host-gateway \
+#do
+#  echo "Waiting for postgres"
+#  sleep 2;
+#done
 
 echo "docker is now running"
 docker-compose down
